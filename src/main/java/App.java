b020362aba493f2ec8cfa91ac9260b88f06ee0f6
kevinwairi <254wairi@gmail.com>
2018-10-05 14:32:38 +0300

    Import scanner and initialize to get user input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,16 @@
+import java.util.Scanner;
+
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Encrypt isEncrypt =new Encrypt();
-        Decrypt isDecrypt =new Decrypt();
+        Encrypt isEncrypt = new Encrypt();
+        Decrypt isDecrypt = new Decrypt();
 
+        Scanner text = new Scanner(System.in);
+        System.out.println("Please enter the text to encrypt");
+        String plainText = text.next();
+        System.out.println(isEncrypt.encrypt(plainText, 2));
 
-        String textEnc = "This is some message";
-        String Cipherin = isEncrypt.encrypt(textEnc,5);
-        System.out.println(Cipherin);
-
-        String textDec = "ymnx nx xtrj rjxxflj";
-        String Cipherout = isDecrypt.decrypt(textDec,5);
-        System.out.println(Cipherout);
     }
 }
