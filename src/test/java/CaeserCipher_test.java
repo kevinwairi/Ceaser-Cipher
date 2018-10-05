968547e21b1dbe37cfda11ef904ce47a9924920c
kevinwairi <254wairi@gmail.com>
2018-10-05 12:23:48 +0300

    Add a test for Decryption and was pass

--- src/test/java/CaeserCipher_test.java
+++ src/test/java/CaeserCipher_test.java
@@ -5,9 +5,15 @@ import static org.junit.Assert.assertEquals;
 public class CaeserCipher_test {
 
     private Encrypt testEncrypt = new Encrypt();
+    private Decrypt testDecrypt = new Decrypt();
 
     @Test
-    public void test(){
+    public void encryptdata(){
         assertEquals( "id", testEncrypt.encrypt("gb",2));
+    }
+
+    @Test
+    public void decryptdata(){
+        assertEquals( "gb", testDecrypt.decrypt("id",2));
     }
 }
