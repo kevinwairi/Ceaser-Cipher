c30c8c9e055fb68504bc9fadcacb1d20c6709289
kevinwairi <254wairi@gmail.com>
2018-10-05 12:29:49 +0300

    Call to the main message the encrypt function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,12 @@ public class App {
 
     public static void main(String[] args){
 
+        Encrypt isEncrypt =new Encrypt();
+
+
+        String textEnc = "This is some message";
+        String Cipherin = isEncrypt.encrypt(textEnc,5);
+        System.out.println(Cipherin);
 
     }
 }
