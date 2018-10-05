bc718bd0d992756221321103488c8fa5cb6db7d4
kevinwairi <254wairi@gmail.com>
2018-10-05 12:32:43 +0300

    Call function Decrypt in App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,15 @@ public class App {
     public static void main(String[] args){
 
         Encrypt isEncrypt =new Encrypt();
+        Decrypt isDecrypt =new Decrypt();
 
 
         String textEnc = "This is some message";
         String Cipherin = isEncrypt.encrypt(textEnc,5);
         System.out.println(Cipherin);
 
+        String textDec = "ymnx nx xtrj rjxxflj";
+        String Cipherout = isDecrypt.decrypt(textDec,5);
+        System.out.println(Cipherout);
     }
 }
