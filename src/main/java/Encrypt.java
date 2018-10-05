d4d8fa2abe31088e8b58c6bf64f499311d30ad7f
kevinwairi <254wairi@gmail.com>
2018-10-05 10:20:24 +0300

    Add an if statement that checks if shift is greater than 26 and do the modulus

---
+++ src/main/java/Encrypt.java
@@ -0,0 +1,10 @@
+public class Encrypt {
+
+    public static String encrypt(String plainText, int shift) {
+        if (shift > 26) {
+            shift = shift % 26;
+        } else if (shift < 0) {
+            shift = (shift % 26) + 26;
+        }
+    }
+}
